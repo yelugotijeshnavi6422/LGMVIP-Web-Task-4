7acfa20674b168e39ae8004146c0488ff289d008
YELUGOTI JESHNAVI <144583169+yelugotijeshnavi6422@users.noreply.github.com>
2023-11-05 18:11:04 +0530

    style.js

--- style.js
+++ style.js
@@ -16,10 +16,3 @@ function clearDisplay() {
     const display = document.getElementById("display");
     display.value = "";
 }
-
-isplay.value =3D &quot;Error&quot;;<br>=C2=A0 =C2=A0 }<br>}<br><br>function=
- clearDisplay() {<br>=C2=A0 =C2=A0 const display =3D document.getElementByI=
-d(&quot;display&quot;);<br>=C2=A0 =C2=A0 display.value =3D &quot;&quot;;<br=
->}<br></div>
-
---000000000000ececd2060966b9ac--
